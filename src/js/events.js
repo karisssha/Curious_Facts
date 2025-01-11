2d3d5f24c8d2f4fe4938a7f6f4d5b7015b7ceb0d
catmaluci <catmaluci@hotmail.com>
2025-01-11 17:26:13 +0100

    changing html to reuse one layout for multiple screens

--- src/js/events.js
+++ src/js/events.js
@@ -3,11 +3,44 @@ import { changeRandomFact } from "./functions.js"
 import { addFavourite } from "./favourites.js";
 // evento click API
 
+function showRandomFact (){
+    getRandomFact();
+    const curiosityScreen = document.querySelector ('#id-curiosity-screen');
+    const fact = document.querySelector ('#fact-API');
+    const favouriteFacts = document.querySelector ('#favourite-facts');
+    const kitty = document.querySelector ('#facts-cat');
+
+    fact.classList.remove ('hidden');
+    favouriteFacts.classList.add ('hidden');
+    curiosityScreen.classList.remove ('favourite');
+    kitty.setAttribute('src', '/assets/img/cat_splash.svg');
+}
+
 const clickNextFact = document.querySelector("#next-fact-btn");
-clickNextFact.addEventListener("click", getRandomFact, changeRandomFact)
+clickNextFact.addEventListener("click", showRandomFact)
 
 
 // event to add a favourite
 
 const clickAddFavourite = document.querySelector('#id_addToFavourite');
 clickAddFavourite.addEventListener("click", addFavourite)
+
+
+//function for showing and hidden screens
+
+function showFavourites (){
+    const curiosityScreen = document.querySelector ('#id-curiosity-screen');
+    const fact = document.querySelector ('#fact-API');
+    const favouriteFacts = document.querySelector ('#favourite-facts');
+    const kitty = document.querySelector ('#facts-cat');
+
+    fact.classList.add ('hidden');
+    favouriteFacts.classList.remove ('hidden');
+    curiosityScreen.classList.add ('favourite');
+    kitty.setAttribute('src', '/assets/img/cat_favourites.svg');
+}
+
+// event to go to favourite
+
+const clickgoToFavourite = document.querySelector('#id_goToFavourites');
+clickgoToFavourite.addEventListener("click", showFavourites)
