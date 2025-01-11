08444f7602dc0d4abe15a4cfa3b2aa8ff99837e4
catmaluci <catmaluci@hotmail.com>
2025-01-11 16:14:32 +0100

    adding favourites to localstorage

--- src/js/favourites.js
+++ src/js/favourites.js
@@ -1,5 +1,15 @@
+export const getFavourites = function (){
+    return JSON.parse(localStorage.getItem('favourites') ?? "{}");
+}
+
 
 export const addFavourite = function (){
-    localStorage.setItem('myCat', 'Tobi');
+    let randomFactValue = document.querySelector("#fact-API").textContent;
+    let randomFactIdValue = document.querySelector("#fact-API-id").textContent;
+
+    let favourites = getFavourites();
+    favourites[randomFactIdValue] = randomFactValue
+
+    localStorage.setItem('favourites', JSON.stringify(favourites));
 }
 
