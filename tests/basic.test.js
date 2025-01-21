67ec95fedcbdbe9e530f97845e8dabc61957c809
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-01-21 09:56:42 +0100

    test retouch

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,25 +1,43 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeAll, test } from "vitest";
 import { getRandomFact } from "../src/js/api";
 import { addFavourite } from "../src/js/favourites";
-import { changeRandomFact } from "../src/js/functions";
-
-
-
-
 
 describe("Start", () => {
-    it("should be declared", () => {
+    it('should be declared', () => {
         expect(typeof getRandomFact).toBe('function');
     });
-    it("should show a fact", () => {
+    it('should show a fact', () => {
         expect(getRandomFact).not.toBeNull();
     });
 });
+
 describe("Second Page", () => {
-    it("should be declared", () => {
+    it('should be declared', () => {
         expect(typeof addFavourite).toBe('function');
     });
-    it("should add a fact", () => {
+    it('should add a fact', () => {
         expect(addFavourite).not.toBeNull();
     });
 })
+
+describe("API Call", () => {
+
+    const BEFORE_ALL_TIMEOUT = 30000;
+    let response = Response;
+    let body;
+    
+    beforeAll(async () => {
+        response = await fetch(
+          'https://uselessfacts.jsph.pl/api/v2/facts/random',
+        );
+        body = response.json();
+        }, BEFORE_ALL_TIMEOUT);
+    
+        test('should have response status 200', () => {
+            expect(response.status).toBe(200);
+      });
+         test('should have content-type', () => {
+            expect(response.headers.get('Content-Type')).toBe('application/json; charset=UTF-8');
+      });
+});
+
